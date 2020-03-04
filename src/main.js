1be76c4c3feb46a4b8f4e844a50a570784b397a8
Yang <704799639@qq.com>
2020-03-05 00:06:40 +0800

    完成基本样式和适配布局

--- src/main.js
+++ src/main.js
@@ -3,6 +3,37 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import {
+  Toast,
+  List,
+  Dialog,
+  Stepper,
+  ActionSheet,
+  Overlay,
+  Slider,
+  Tab,
+  Tabs,
+  Popup,
+  Sticky,
+  Swipe,
+  SwipeItem,
+  CountDown
+} from 'vant' // 全局引入vant样式
+
+Vue.use(Toast)
+  .use(List)
+  .use(Dialog)
+  .use(Stepper)
+  .use(ActionSheet)
+  .use(Overlay)
+  .use(Slider)
+  .use(Tab)
+  .use(Tabs)
+  .use(Popup)
+  .use(Sticky)
+  .use(Swipe)
+  .use(SwipeItem)
+  .use(CountDown)
 
 Vue.config.productionTip = false
 
